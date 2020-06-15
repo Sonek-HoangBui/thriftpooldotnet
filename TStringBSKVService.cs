ee3a7d10877f0c9ca40d0d6ee550ab002de760db
nguyenminhduc285 <duccoloa98@gmail.com>
2020-06-15 14:24:26 +0700

    add lib

--- TStringBSKVService.cs
+++ TStringBSKVService.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading;
 using org.openstars.core.bigset.Generic;
 
 namespace ThriftPoolDotNet
@@ -5,14 +7,44 @@ namespace ThriftPoolDotNet
     public class TStringBSKVService
     {
         private static BigSetClient _bigSetClient;
-
-        public TStringBSKVService()
+        private static TStringBigSetKVService.Client _aClient;
+        private static readonly object _object = new object();
+        public TStringBSKVService(BigSetClient bigSetClient)
         {
-            
+            _bigSetClient = bigSetClient;
         }
-        public TErrorCode BsgPutItem(string key, TItem item)
+        public bool BsgPutItem(string key, TItem item)
         {
-            
+            lock (_object)
+            {
+                TClientInfo clientInfo = _bigSetClient.getClient();
+                if (!clientInfo.isOpen())
+                {
+                    clientInfo.doOpen();
+                }
+
+                if (!clientInfo.sureOpen())
+                {
+                    Thread.Sleep(1 * 100);
+                    if (!clientInfo.sureOpen())
+                    {
+                        Console.WriteLine("Can't open");
+                        return false;
+                    }
+                }
+
+                object tmp = clientInfo.getClient();
+                _aClient = (TStringBigSetKVService.Client) tmp;
+                var bsPutItemAsync = _aClient.bsPutItemAsync(key, item);
+
+                if (bsPutItemAsync.IsCompleted == false)
+                {
+                    bsPutItemAsync.Wait();
+                }
+
+                clientInfo.cleanUp();
+                return true;
+            }
         }
     }
 }
